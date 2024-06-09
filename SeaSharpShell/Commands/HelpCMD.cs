0a1d8508d34c3c357d649100b57a015a6a2b4889
Andreas Johansson <glanshammar70@gmail.com>
2024-06-09 14:34:15 +0200

    Made file for Help command.

---
+++ SeaSharpShell/Commands/HelpCMD.cs
@@ -0,0 +1,15 @@
+﻿namespace SeaSharpShell.Commands;
+
+public class HelpCMD
+{
+    public static void ShowHelp()
+    {
+        Console.WriteLine("Available commands:");
+        Console.WriteLine("help - Show this help message");
+        Console.WriteLine("hello - Print 'Hello World!'");
+        Console.WriteLine("age - Calculate your age");
+        Console.WriteLine("clear - Clear the console screen");
+        Console.WriteLine("quit - Exit the program");
+        Console.WriteLine("exit - Exit the program");
+    }
+}
