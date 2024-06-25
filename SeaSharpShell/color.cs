8bcbcc1fbeeec393e10c220b1801fd8ca6390fb9
Andreas Johansson <glanshammar70@gmail.com>
2024-06-25 13:25:38 +0200

    Added Color functionality.

---
+++ SeaSharpShell/color.cs
@@ -0,0 +1,48 @@
+﻿namespace SeaSharpShell;
+
+
+public enum Color
+{
+    DEFAULT,
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    CYAN,
+    MAGENTA,
+    WHITE
+}
+
+public static class ColorHelper
+{
+    public static void SetColor(Color color)
+    {
+        switch (color)
+        {
+            case Color.DEFAULT:
+                Console.ResetColor();
+                break;
+            case Color.RED:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case Color.GREEN:
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+            case Color.BLUE:
+                Console.ForegroundColor = ConsoleColor.Blue;
+                break;
+            case Color.YELLOW:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case Color.CYAN:
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+            case Color.MAGENTA:
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                break;
+            case Color.WHITE:
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+        }
+    }
+}
