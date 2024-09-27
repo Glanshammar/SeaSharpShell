9441ad337e6726e4a3df3659d7b2eac3e7bb5cd5
Andreas Johansson <glanshammar70@gmail.com>
2024-09-27 20:48:27 +0200

    Moved the commands to this new file.

---
+++ SeaSharpShell/CommandList.cs
@@ -0,0 +1,27 @@
+﻿namespace SeaSharpShell;
+using SeaSharpShell.Commands;
+
+public class CommandList
+{
+    public delegate void CommandAction(params string[] args);
+
+    public static Dictionary<string, CommandAction> Commands { get; } = new Dictionary<string, CommandAction>()
+    {
+        { "help", (args) => HelpCMD.ShowHelp() },
+        { "age", (args) => AgeCMD.Age() },
+        { "ls", (args) => Filesystem.ListFiles() },
+        { "cd", (args) => Filesystem.ChangeDirectory(args) },
+        { "mkdir", (args) => Filesystem.CreateDirectory(args) },
+        { "rmdir", (args) => Filesystem.DeleteDirectory(args) },
+        { "touch", (args) => Filesystem.CreateFile(args) },
+        { "read", (args) => Filesystem.ReadFile(args) },
+        { "rename", (args) => Filesystem.RenameFile(args) },
+        { "mv", (args) => Filesystem.MoveFile(args) },
+        { "cp", (args) => Filesystem.CopyFile(args) },
+        { "delete", (args) => Filesystem.DeleteFile(args) },
+        { "clear", (args) => Console.Clear() },
+        { "cls", (args) => Console.Clear() },
+        { "quit", (args) => Environment.Exit(0) },
+        { "exit", (args) => Environment.Exit(0) },
+    };
+}
