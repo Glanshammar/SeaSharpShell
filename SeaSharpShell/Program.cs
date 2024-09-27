aa0dc8c74421af94a5dd92eb50e06ad91f60bb22
Andreas Johansson <glanshammar70@gmail.com>
2024-09-27 20:49:17 +0200

    Pruned the code after moving the command dict.

--- SeaSharpShell/Program.cs
+++ SeaSharpShell/Program.cs
@@ -4,32 +4,10 @@ using SeaSharpShell;
 
 class Program
 {
-    delegate void CommandAction(params string[] args);
     
     static void Main(string[] args)
     {
         Console.Title = "SeaSharpShell";
-        
-        Dictionary<string, CommandAction> commandActions = new Dictionary<string, CommandAction>()
-        {
-            { "help", (args) => HelpCMD.ShowHelp() },
-            { "age", (args) => AgeCMD.Age() },
-            { "ls", (args) => Filesystem.ListFiles() },
-            { "cd", (args) => Filesystem.ChangeDirectory(args) },
-            { "mkdir", (args) => Filesystem.CreateDirectory(args) },
-            { "rmdir", (args) => Filesystem.DeleteDirectory(args) },
-            { "touch", (args) => Filesystem.CreateFile(args) },
-            { "read", (args) => Filesystem.ReadFile(args) },
-            { "rename", (args) => Filesystem.RenameFile(args) },
-            { "mv", (args) => Filesystem.MoveFile(args) },
-            { "cp", (args) => Filesystem.CopyFile(args) },
-            { "delete", (args) => Filesystem.DeleteFile(args) },
-            { "clear", (args) => Console.Clear() },
-            { "cls", (args) => Console.Clear() },
-            { "quit", (args) => Environment.Exit(0) },
-            { "exit", (args) => Environment.Exit(0) },
-        };
-        
 
         while (true)
         {
@@ -46,9 +24,9 @@ class Program
                 string command = inputParts[0].ToLower();
                 string[] commandArgs = inputParts.Length > 1 ? inputParts[1].Split(' ') : new string[0]; // Extract command arguments
 
-                if (commandActions.ContainsKey(command))
+                if (CommandList.Commands.ContainsKey(command))
                 {
-                    commandActions[command].Invoke(commandArgs); // Pass the command arguments
+                    CommandList.Commands[command].Invoke(commandArgs);
                 }
                 else
                 {
